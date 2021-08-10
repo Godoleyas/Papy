aaa73c07de69fd9754b44dd0f7962fa64389a085
Godoleyas <papygamer321@gmail.com>
2021-08-10 15:35:52 +0300

    Scroll Function Added

--- ESPORT/js/app.js
+++ ESPORT/js/app.js
@@ -15,3 +15,26 @@ function alranacc() {
     signuptxt.innerHTML = 'Sign In';
     console.log('Sign in');
 }
+
+
+
+
+//Get the button
+var mybutton = document.getElementById("Scrollbtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
